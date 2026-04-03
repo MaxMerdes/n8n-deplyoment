a205fcb66b45ce1e913872cf3fdb1e78d7cd6143
Max Merdes <mail@max-merdes.de>
2026-04-03 21:21:59 +0200

    added values to n8n-webhook.sh

--- Integrations/Wazuh/n8n-webhook.sh
+++ Integrations/Wazuh/n8n-webhook.sh
@@ -3,18 +3,24 @@
 # JSON vom Event lesen
 read INPUT_JSON
 
-# Beispiel: Felder extrahieren (jq muss installiert sein)
-SRC_IP=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.data.srcip')
+# Felder extrahieren
 RULE_ID=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.rule.id')
-LEVEL=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.rule.level')
+RULE_LEVEL=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.rule.level')
+RULE_DESC=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.rule.description')
+AGENT_ID=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.agent.id')
+AGENT_NAME=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.agent.name')
+SRC_IP=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.data.srcip // empty')
 FULL_LOG=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.full_log')
 
-# Curl Request senden
+# Curl Request an n8n oder direkt an PostgreSQL Webhook
 curl -X POST https://dein-endpoint/api \
   -H "Content-Type: application/json" \
   -d "{
-    \"src_ip\": \"$SRC_IP\",
-    \"rule_id\": \"$RULE_ID\",
-    \"level\": \"$LEVEL\",
-    \"log\": \"$FULL_LOG\"
-  }"
+        \"rule_id\": \"$RULE_ID\",
+        \"rule_level\": $RULE_LEVEL,
+        \"rule_description\": \"$RULE_DESC\",
+        \"agent_id\": \"$AGENT_ID\",
+        \"agent_name\": \"$AGENT_NAME\",
+        \"src_ip\": \"$SRC_IP\",
+        \"full_log\": \"$FULL_LOG\"
+      }"
